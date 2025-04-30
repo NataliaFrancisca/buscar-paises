894b7ddb7e16be7a78ff7eadad5102856687b782
nataliafrancisca <nathifrancisca@gmail.com>
2025-04-29 22:22:04 -0300

    feat: troquei o tipo da propriedade flags

--- src/main/java/br/com/nat/modelos/PaisDTO.java
+++ src/main/java/br/com/nat/modelos/PaisDTO.java
@@ -3,4 +3,4 @@ package br.com.nat.modelos;
 import java.util.List;
 import java.util.Map;
 
-public record PaisDTO (NomeDTO name, List<String> capital, String subregion, Map<String, String> languages, Object flags){}
+public record PaisDTO (NomeDTO name, List<String> capital, String subregion, Map<String, String> languages, Map<String, String> flags){}
